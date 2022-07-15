b95cc2ba640adab06db8b574e21ec52d8f998755
Matheus Souza <matheus.ramirez019@gmail.com>
2022-07-15 17:44:05 -0300

    update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,38 @@ VALUES('In101', 'Starter','Inglês','basico','matutino');
 
 INSERT INTO curso (codigo,nome,idioma,nivel,turno)
 VALUES('Es101', 'Iniciante','Espanhol','basico','matutino');
+
+INSERT INTO aluno (nome,curso_id,idade , email, telefone )
+VALUES('Joao',1,18,'mail@mail.com', '999999999');
+
+INSERT INTO aluno (nome,curso_id,idade , email, telefone)
+VALUES('Maria',1,18,'mail@mail.com','999999999');
+
+INSERT INTO aluno (nome,curso_id,idade , email, telefone , nome_responsavel, email_responsavel, telefone_responsavel)
+VALUES('Joaquin',1,16,'mail@mail.com', '999999999','Pai Joaquin', 'responsavel@mail.com', '888888888');
+
+INSERT INTO aluno (nome,curso_id,idade , email, telefone , nome_responsavel, email_responsavel, telefone_responsavel)
+VALUES('Joana',1,12,'mail@mail.com', '999999999','Pai Joana', 'responsavel@mail.com', '888888888');
+
+INSERT INTO aluno (nome,curso_id,idade , email, telefone)
+VALUES('Mariana',1,45,'mail@mail.com', '999999999');
+
+INSERT INTO aluno (nome,curso_id,idade , email, telefone )
+VALUES('Julio',2,18,'mail@mail.com', '999999999');
+
+INSERT INTO aluno (nome,curso_id,idade , email, telefone )
+VALUES('Henrique',2,18,'mail@mail.com', '999999999');
+
+INSERT INTO aluno (nome,curso_id,idade , email, telefone , nome_responsavel, email_responsavel, telefone_responsavel)
+VALUES('Margarida',2,13,'mail@mail.com', '999999999','Pai Margarida', 'responsavel@mail.com', '888888888');
+
+INSERT INTO aluno (nome,curso_id,idade , email, telefone , nome_responsavel, email_responsavel, telefone_responsavel)
+VALUES('Taldo',2,12,'mail@mail.com','999999999','Pai Taldo', 'responsavel@mail.com', '888888888');
+
+INSERT INTO aluno (nome,curso_id,idade , email, telefone)
+VALUES('Sebastiao',2,45,'mail@mail.com', '999999999');
+
+INSERT INTO professor(nome,sobrenome,curso_id,turno , email, telefone)
+VALUES('Sebastiao','Gonzaga',2,'matutino','mail@mail.com', '999999999');
+INSERT INTO professor(nome,sobrenome,curso_id,turno , email, telefone)
+VALUES('Sebastiao','Gonzaga',2,'matutino','mail@mail.com', '999999999');
